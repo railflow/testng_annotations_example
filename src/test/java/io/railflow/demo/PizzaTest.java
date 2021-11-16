c4299d62c8cc435a8ec272be66ec3c528e6c301c
Sergey Oplavin <sergey.oplavin@agiletestware.com>
2021-11-16 16:36:36 +0300

    Add PizzaTest example

---
+++ src/test/java/io/railflow/demo/PizzaTest.java
@@ -0,0 +1,90 @@
+package io.railflow.demo;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.*;
+
+import io.railflow.annotations.CustomField;
+import io.railflow.annotations.Railflow;
+import io.railflow.annotations.testng.CurrentTest;
+import io.railflow.annotations.testng.RailflowReporter;
+
+/**
+ * Pizza tests.
+ * 
+ * @author Sergey Oplavin
+ */
+@Railflow(title = "Pizza tests", caseType = "Railflow", casePriority = "High", caseFields = {
+		@CustomField(name = "Required text field", value = "Hello from Railflow"),
+		@CustomField(name = "estimate", value = "42s") }, resultFields = {
+				@CustomField(name = "Custom field", value = "Results from Railflow"),
+				@CustomField(name = "version", value = "1.0") }, smartFailureAssignment = "user1@yourcompany.com")
+@Listeners(RailflowReporter.class)
+public class PizzaTest {
+	private static final String GOOGLE = "https://google.com";
+	private final RemoteWebDriver webDriver = new ChromeDriver(new ChromeOptions().addArguments("--start-fullscreen"));
+
+	@BeforeClass
+	public static void setUp() {
+		System.setProperty("webdriver.chrome.driver", new File("chromedriver.exe").getAbsolutePath());
+	}
+
+	@Railflow(title = "Find some pizzas")
+	@Test
+	public void find_some_pizzas() {
+		this.webDriver.navigate().to(GOOGLE);
+		final List<WebElement> results = getGoogleResults("pizza");
+		assertTrue("Cannot find any pizzas", results.size() > 0);
+	}
+
+	@Railflow(testrailIds = 13390)
+	@Test
+	public void there_are_no_bad_pizzas() {
+		this.webDriver.navigate().to(GOOGLE);
+		final List<WebElement> results = getGoogleResults("bad pizza");
+		assertEquals(results.size(), 0, "Liar! There are no bad pizzas!!!");
+	}
+
+	@AfterMethod
+	public void afterMethod(final ITestResult result) throws IOException {
+		if (ITestResult.FAILURE == result.getStatus()) {
+			final byte[] screenshot = this.webDriver.getScreenshotAs(OutputType.BYTES);
+			final ITestResult currentTestResult = Reporter.getCurrentTestResult();
+			try {
+				Reporter.setCurrentTestResult(result);
+				CurrentTest.addAttachment(result.getName() + "_failure.png", screenshot);
+			} finally {
+				Reporter.setCurrentTestResult(currentTestResult);
+			}
+		}
+	}
+
+	@AfterTest
+	public void afterTest() {
+		this.webDriver.quit();
+	}
+
+	private List<WebElement> getGoogleResults(final String query) {
+		final WebElement element = this.webDriver.findElement(By.name("q"));
+		element.sendKeys(query);
+		element.submit();
+		new WebDriverWait(this.webDriver, 1)
+				.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("search")));
+		return this.webDriver.findElements(By.xpath("//*[@id='rso']//*[@class='g']//a"));
+	}
+}
